ff4142c6a8d31e18f8657d8993e346f93f92c173
Mako <mgabriel.alvarado10@gmail.com>
2025-02-28 14:58:27 -0400

    agregando menu y estilos

--- orquesta/src/App.jsx
+++ orquesta/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Route, Routes, BrowserRouter } from "react-router-dom"
 import Login from  './pages/auth/login'
+import Menu from './pages/menu/menu'
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}>
-          {/*<Route path="*" element={<NoPage />} />*/}
-        </Route>
+        {/* Rutas independientes (no anidadas) */}
+        <Route path="/" element={<Login />} />
+        <Route path="/menu" element={<Menu />} />
       </Routes>
     </BrowserRouter>
   )
