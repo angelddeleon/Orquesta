173061b45f80f58b97c8742a8ba69b65602f579e
cesardarizaleta <cesardarizaleta@gmail.com>
2025-02-27 21:30:47 -0400

    Agregando Bootstrap y Router

--- orquesta/src/App.jsx
+++ orquesta/src/App.jsx
@@ -1,14 +1,17 @@
-
-import './App.css'
+import React from 'react'
+import { Route, Routes, BrowserRouter } from "react-router-dom"
+import Login from  './pages/auth/login'
 
 function App() {
 
   return (
-    <>
-
-    <h1>Frontend Orquesta Sinfonica</h1>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}>
+          {/*<Route path="*" element={<NoPage />} />*/}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
