63522a73e9933228be499b887ecf97802d5e8ccd
cesardarizaleta <cesardarizaleta@gmail.com>
2025-02-27 21:46:44 -0400

    Agregando Layout

---
+++ orquesta/src/layout/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Layout({ children }) {
+    return (
+        <main className='vw-100 vh-100'>
+            {children}
+        </main>
+    )
+}
--- orquesta/src/pages/auth/login.tsx
+++ orquesta/src/pages/auth/login.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Layout from '../../layout/layout';
 
 export default function Login(){
     return (
-        <div>
+        <Layout>
             <h1>Login</h1>
-        </div>
+        </Layout>
     )
 }
