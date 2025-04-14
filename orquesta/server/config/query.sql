8ec714d4b4f832ade7ebb228f2cd4a3a612774d1
cesardarizaleta <cesardarizaleta@gmail.com>
2025-04-14 13:55:57 -0400

    Primeros 500ln de la query

---
+++ orquesta/server/config/query.sql
@@ -0,0 +1,1191 @@
+
+-- 0 - 508
+-- Fila 1
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Lejania', 
+  'N/A', 
+  'A-001', 
+  'Sede1', 
+  'Abreu Anibal', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 2
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Chavez Vivira Contigo', 
+  'N/A', 
+  'A-001', 
+  'Sede1', 
+  'Acedo Omar', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 3
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Danza Venezolanas', 
+  'N/A', 
+  'A-001', 
+  'Sede1', 
+  'Acosta Omar', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'Violin C con copia y original', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 4
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Felicidad', 
+  'N/A', 
+  'A-001', 
+  'Sede1', 
+  'Aguirre Oswaldo', 
+  'N/A', 
+  'Rafael Rodriguez', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 5
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Album Sonoro #3', 
+  'N/A', 
+  'A-001', 
+  'Sede1', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'Piano', 
+  'N/A', 
+  FALSE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 6
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'La Campana de San Justo', 
+  'N/A', 
+  'A-001', 
+  'Sede1', 
+  'Arora Colombino', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 7
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Tu Sonrisa', 
+  'N/A', 
+  'A-001', 
+  'Sede1', 
+  'Cooke / Ray', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 8
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Torna a Sorrento', 
+  'N/A', 
+  'A-001', 
+  'Sede1', 
+  'Curtis', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 9
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Te quiero dijiste muñeca linda', 
+  'N/A', 
+  'A-001', 
+  'Sede1', 
+  'Grever Maria', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 10
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Amor Indio', 
+  'N/A', 
+  'A-001', 
+  'Sede1', 
+  'Mixto Felipe', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 11
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'No se por que', 
+  'N/A', 
+  'A-001', 
+  'Sede1', 
+  'Onix / Sentix', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 12
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Si vous L avinez', 
+  'N/A', 
+  'A-001', 
+  'Sede1', 
+  'Sandoval Luis', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 13
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Tango in D', 
+  'N/A', 
+  'A-001', 
+  'Sede1', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 14
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Cadiz', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albaniz .', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 15
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Midsummer nights serenade Op 232', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albaniz .', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'Gran Casa', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 16
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Midsummer Nights Serenade ', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albaniz .', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'Gran Casa', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 17
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Navarra ', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albaniz .', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'Gran Casa', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 18
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Tango', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albeniz .', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'Gran Casa', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 19
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Triana de la Suite Iberia', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albeniz .', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'Tai Scrre', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 20
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Iberia Suite La Fete Dieu a Sevilla', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albeniz .', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'Gran Casa', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 21
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Nocturno for Violoncello y Guitarra', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albeniz .', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'Gran Casa', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 22
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Sevilla', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albeniz ', 
+  'Guenther Felix', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 23
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Navarra', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albeniz .', 
+  'Langey Otto', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  TRUE, 
+  'Hay 2 partituras para tambores y campanas (Perc (Snare & Bells))', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 24
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Midsummer Night\'s Serenade (Op. 232)', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albeniz .', 
+  'Artok, Von L. Kierfert, C.', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  TRUE, 
+  'X hay obra, solo smire', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 25
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Tango Tango in D', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albeniz .', 
+  'Sanford Harold', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'Violin C con copia y originsa', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 26
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Cadiz (Saeta)', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albeniz .', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  TRUE, 
+  'N/A', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 27
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Córdoba (Nocturne) de ''Chants d''Espagne'',opus 232', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albeniz .', 
+  'N/A', 
+  'Fernandez Arbós, E.', 
+  'N/A', 
+  'N/A', 
+  TRUE, 
+  'Hay 2 partituras opus para tambores y campanas (Perc (Snare & Bells))', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 28
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'La Fête Dieu a Sevilla (de la Suite ''Iberia'')', 
+  'N/A', 
+  'A-002', 
+  'Sede2', 
+  'Albeniz .', 
+  'N/A', 
+  'Fernandez Arbós, E.', 
+  'N/A', 
+  'N/A', 
+  TRUE, 
+  'Hay 2 partituras para tambores y campanas (Perc (Snare & Bells))', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+
+
+
+
+-- Fila 30
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Concerto á 5, Op. 9 N°8', 
+  'N/A', 
+  'A-003', 
+  'Sede3', 
+  'Albinoni Tomaso', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  TRUE, 
+  'No hay obra, solo score', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 31
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Concierto A5 OP. 9 N°2', 
+  'N/A', 
+  'A-003', 
+  'Sede3', 
+  'Albinoni Tomaso', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  TRUE, 
+  'No hay obra, solo score', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 32
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Sonate en Sol Mineur', 
+  'N/A', 
+  'A-003', 
+  'Sede3', 
+  'Albinoni Tomaso', 
+  'Thilde Jean', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'Sin Score', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 33
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Concierto AS OP. 7 N°5', 
+  'N/A', 
+  'A-003', 
+  'Sede3', 
+  'Albinoni Tomaso', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'Si hay obra, solo score', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 34
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Adagio in sol minore', 
+  'N/A', 
+  'A-003', 
+  'Sede3', 
+  'Albinoni Tomaso', 
+  'Giazotto Remo', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  TRUE, 
+  'Contrabbasso 5 completas y 1 incompleta', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 35
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Trio per due violini e violoncello', 
+  'N/A', 
+  'A-003', 
+  'Sede3', 
+  'Albrechstsberger Johann Georg', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  TRUE, 
+  'Tii Scrre', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 36
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Concertino in Eb a cinque', 
+  'N/A', 
+  'A-003', 
+  'Sede3', 
+  'Albrechtsberger Johann Georg', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  TRUE, 
+  'Con Score', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 37
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Te Quiero, Te Quiero', 
+  'N/A', 
+  'A-003', 
+  'Sede3', 
+  'Alquero Augusto', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'Violin 2 : 3 completas y 2 incompletas', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- Fila 38
+INSERT INTO partituras (
+  obra, 
+  archivista, 
+  caja, 
+  sede, 
+  compositor, 
+  arreglista, 
+  orquestacion, 
+  formato, 
+  categoria, 
+  score, 
+  observaciones, 
+  createdAt, 
+  updatedAt
+) VALUES 
+(
+  'Alma Llanera', 
+  'N/A', 
+  'A-004', 
+  'Sede4', 
+  'Gutierrez Pedro', 
+  'Desconocido', 
+  'N/A', 
+  'N/A', 
+  'N/A', 
+  FALSE, 
+  'Autor desconocido', 
+  '2023-04-14 12:02:00', 
+  '2023-04-14 12:02:00'
+);
+
+-- 508 - 1002
